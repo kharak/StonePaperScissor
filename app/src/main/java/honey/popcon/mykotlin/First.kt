2ee41247dd787e38fb044bb44ef0f4ed779a57c8
kharak <secretesword@gmail.com>
2018-01-28 19:35:24 +0530

    first commit bro

---
+++ app/src/main/java/honey/popcon/mykotlin/First.kt
@@ -0,0 +1,10 @@
+package honey.popcon.mykotlin
+
+/**
+ * Created by Horizon on 1/6/2018.
+ */
+class First {
+    fun main(args :Array<String>){
+        println("Hell kotlin")
+    }
+}
---
+++ app/src/main/java/honey/popcon/mykotlin/Learn.kt
@@ -0,0 +1,8 @@
+package honey.popcon.mykotlin
+
+/**
+ * Created by Horizon on 1/6/2018.
+ */
+fun main(args :Array<String>){
+    println("Hell kotlin")
+}
